834ffee8834864941db73928008e55cc6c25a6b5
JaDarius Flagg <jadariusflagg@gmail.com>
2024-09-20 10:46:21 -0500

    Add files via upload

---
+++ src/main/java/com/idearefinery/assignment/TaskassignmentApplication.java
@@ -0,0 +1,12 @@
+package com.idearefinery.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskassignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskassignmentApplication.class, args);
+	}
+}
---
+++ src/test/java/com/idearefinery/assignment/TaskassignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idearefinery.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskassignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
